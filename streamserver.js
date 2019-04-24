c0db87959cedadaaa656fdc9da8c7fffd0304c7d
RaulJimenezOrtega <hhkaos@gmail.com>
2019-04-25 01:02:02 +0200

    Adding stream server

--- streamserver.js
+++ streamserver.js
@@ -3,6 +3,59 @@ var app = express();
 var expressWs = require('express-ws')(app);
 var cors = require('express-cors');
 
+var websocket = require('websocket-stream')
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(9000, function() {
+    console.log((new Date()) + ' Server is listening on port 9000');
+});
+
+var wss = websocket.createServer({
+  perMessageDeflate: false,
+  server: server
+}, handle)
+
+function handle(stream) {
+  console.log("Entramos:", new Date())
+  stream.on('data', (chunk) => {
+      console.log('Event data:', new Date());
+      console.log(chunk.toString());
+  });
+  stream.on('close', (chunk) => {
+      console.log('Event close:', new Date());
+  });
+  stream.on('end', (chunk) => {
+      console.log('Event end:', new Date());
+  });
+  stream.on('error', (chunk) => {
+      console.log('Event error:', new Date());
+  });
+  stream.on('pause', (chunk) => {
+      console.log('Event pause:', new Date());
+  });
+  stream.on('readable', (chunk) => {
+      console.log('Event readable:', new Date());
+  });
+  stream.on('resume', (chunk) => {
+      console.log('Event resume:', new Date());
+  });
+}
+
+function decodeBuffer(message){
+    var json = JSON.stringify(message.binaryData);
+    var bufferOriginal = Buffer.from(JSON.parse(json).data);
+    var msg = bufferOriginal.toString('utf8');
+
+    return msg;
+}
+
 var mock  = {
     messages: './mock/messages.json',
     serviceDesc: './mock/service.js'
@@ -17,11 +70,13 @@ const TIME_INTERVAL = 2000;
 const NGROK = process.argv[2];
 const ENDPOINT = "/arcgis/rest/services/ASDITrackInformation/StreamServer";
 
-app.use(cors({
-    allowedOrigins: [
-        NGROK, 'localhost:8080'
-    ]
-}));
+app.use(cors())
+//     cors({
+//     allowedOrigins: [
+//         NGROK, 'localhost:8080'
+//     ]
+// })
+// );
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
