95d868ba91b5f50ed3256a8f16ecd5d712398c40
RaulJimenezOrtega <hhkaos@gmail.com>
2019-04-25 15:11:40 +0200

    Adding twitter-rt-service fields

--- mock/service.js
+++ mock/service.js
@@ -1,104 +1,134 @@
 var SERVICE = {
- "description": null,
- "objectIdField": null,
- "displayField": "FltId",
- "timeInfo": {
-  "trackIdField": "FltId",
-  "startTimeField": "MsgTime",
-  "endTimeField": null
- },
- "geometryType": "esriGeometryPoint",
- "geometryField": "Location",
- "spatialReference": {
-  "wkid": 4326,
-  "latestWkid": 4326
- },
- "drawingInfo": {
-  "renderer": {
-      "type": "simple",
-                           "description": "",
-                           "symbol": {
-                               "color": [
-                                   0,
-                                   169,
-                                   230,
-                                   131
-                               ],
-                               "size": 9,
-                               "angle": 0,
-                               "xoffset": 0,
-                               "yoffset": 0,
-                               "type": "esriSMS",
-                               "style": "esriSMSCircle",
-                               "outline": {
-                                   "color": [
-                                       255,
-                                       255,
-                                       255,
-                                       255
-                                   ],
-                                   "width": 1.125,
-                                   "type": "esriSLS",
-                                   "style": "esriSLSSolid"
-                               }
-                           }
-   }
+    "description": null,
+    "objectIdField": null,
+    "displayField": "id_str",
+    "timeInfo": {
+        "trackIdField": "id_str",
+        "startTimeField": "MsgTime",
+        "endTimeField": null
+    },
+    "geometryType": "esriGeometryPoint",
+    "geometryField": "Location",
+    "spatialReference": {
+        "wkid": 4326,
+        "latestWkid": 4326
+    },
+    "drawingInfo": {
+        "renderer": {
+            "type": "simple",
+            "description": "",
+            "symbol": {
+                "color": [
+                    0,
+                    169,
+                    230,
+                    131
+                ],
+                "size": 9,
+                "angle": 0,
+                "xoffset": 0,
+                "yoffset": 0,
+                "type": "esriSMS",
+                "style": "esriSMSCircle",
+                "outline": {
+                    "color": [
+                        255,
+                        255,
+                        255,
+                        255
+                    ],
+                    "width": 1.125,
+                    "type": "esriSLS",
+                    "style": "esriSLSSolid"
+                }
+            }
+        }
 
- },
- "fields": [
-  {
-   "name": "MsgTime",
-   "type": "esriFieldTypeDate",
-   "alias": "MsgTime",
-   "nullable": true
-  },
-  {
-   "name": "DepArpt",
-   "type": "esriFieldTypeString",
-   "alias": "DepArpt",
-   "nullable": true
-  },
-  {
-   "name": "FltId",
-   "type": "esriFieldTypeString",
-   "alias": "FltId",
-   "nullable": true
-  },
-  {
-   "name": "Heading",
-   "type": "esriFieldTypeInteger",
-   "alias": "Heading",
-   "nullable": true
-  },
-  {
-   "name": "AltitudeFeet",
-   "type": "esriFieldTypeInteger",
-   "alias": "AltitudeFeet",
-   "nullable": true
-  },
-  {
-   "name": "FID",
-   "type": "esriFieldTypeInteger",
-   "alias": "FID",
-   "nullable": true
-  }
- ],
- "currentVersion": "10.5",
- "streamUrls": [
+    },
+    "fields": [
+        {
+            "name": "username",
+            "type": "esriFieldTypeString",
+            "alias": "username",
+            "nullable": true
+        },
+        {
+            "name": "screename",
+            "type": "esriFieldTypeString",
+            "alias": "screename",
+            "nullable": true
+        },
+        {
+            "name": "text",
+            "type": "esriFieldTypeString",
+            "alias": "text",
+            "nullable": true
+        },
+        {
+            "name": "profile_image_url_https",
+            "type": "esriFieldTypeString",
+            "alias": "profile_image_url_https",
+            "nullable": true
+        },
+        {
+            "name": "location",
+            "type": "esriFieldTypeString",
+            "alias": "location",
+            "nullable": true
+        },
+        {
+            "name": "created_at",
+            "type": "esriFieldTypeDate",
+            "alias": "created_at",
+            "nullable": true
+        },
+        {
+            "name": "id_str",
+            "type": "esriFieldTypeString",
+            "alias": "id_str",
+            "nullable": true
+        },
+        {
+            "name": "reply_count",
+            "type": "esriFieldTypeString",
+            "alias": "reply_count",
+            "nullable": true
+        },
+        {
+            "name": "retweet_count",
+            "type": "esriFieldTypeString",
+            "alias": "retweet_count",
+            "nullable": true
+        },
+        {
+            "name": "favorite_count",
+            "type": "esriFieldTypeString",
+            "alias": "favorite_count",
+            "nullable": true
+        },
+        {
+            "name": "tweet_url",
+            "type": "esriFieldTypeString",
+            "alias": "tweet_url",
+            "nullable": true
+        }
+    ],
+    "currentVersion": "10.5",
+    "streamUrls": [
 
- ],
- "capabilities": "broadcast,subscribe"
+    ],
+    "capabilities": "broadcast,subscribe"
 }
 
 function fill(wsUrl) {
-  SERVICE.streamUrls.push({
-     "transport": "ws",
-     "urls": [
-         `wss://${wsUrl}`,
-         `ws://${wsUrl}`
-     ]
+    SERVICE.streamUrls.push({
+        "transport": "ws",
+        "urls": [
+            `wss://${wsUrl}`,
+            `ws://${wsUrl}`
+        ]
     });
-  return SERVICE;
+    return SERVICE;
 }
 
 module.exports = fill
